484f0dc452fa265be825ea5d2864404fa658c6d7
OBrettner <oliver.brettner@gmail.com>
2025-08-02 11:15:56 +0200

    Initial Project Setup

---
+++ CraftIOSCodingChallenge/CraftIOSCodingChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CraftIOSCodingChallenge
+//
+//  Created by Oliver Brettmer on 2025. 08. 02..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
